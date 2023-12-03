ae145bbf6bb78af0c73cfa4b5adc157311635628
farhad4new <139057687+farhad4new@users.noreply.github.com>
2023-12-03 15:42:13 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,38 +1,6 @@
 function showLModified() {
 
 let lastModifiedP = document.createElement("p");
-let lModified = new Date(document.lastModified);
-let lMYear = lModified.getFullYear();
-let lMMonth = lModified.getMonth();
-let lMDate = lModified.getDate();
-let lMHours= lModified.getHours();
-let lMMinutes = lModified.getMinutes();
-let lMSeconds = lModified.getSeconds();
-lMHours = addZero(lMHours);
-lMMinutes = addZero(lMMinutes);
-lMSeconds = addZero(lMSeconds);
-
-let now = new Date();
-let thisYear = now.getFullYear();
-let thisMonth = now.getMonth();
-let today = now.getDate();
-now = now.toLocaleString();
-
-if(lMYear == thisYear && lMMonth == thisMonth && lMDate == today){
-lastModifiedP.innerHTML = "<strong> Last Modified: </strong> " + "Today, " + lMHours + ":" + lMMinutes + ":" + lMSeconds;
-} else if(lMYear == thisYear && lMMonth == thisMonth && lMDate == today - 1){
-lastModifiedP.innerHTML = "<strong> Last Modified: </strong> " + "Yesterday, " + lMHours + ":" + lMMinutes + ":" + lMSeconds;
-}
-else{
-lastModifiedP.innerHTML = "<strong> Last Modified: </strong> " + lModified;
-}
+lastModifiedP.innerHTML = document.lastModified;
 document.body.appendChild(lastModifiedP);
 } showLModified();
-
-
-function addZero(i){
-if(i<10){
-i = "0" + i;
-} // add zero in front of numbers < 10
-return i;
-}
